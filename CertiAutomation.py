277309efff391a21706ce921e7c828d36e02d459
Kushan <10797083+kushan02@users.noreply.github.com>
2020-05-03 17:50:35 +0530

    Added support for csv file

--- CertiAutomation.py
+++ CertiAutomation.py
@@ -1,20 +1,18 @@
 # Script Author: Kushan Mehta
-# Target Python version: 3.6+ (Not tested with python 2 version)
-
-# Imports OpenCV library - required for this script
 import cv2
+import pandas as pd
 
 #Modify the below variables according to your preferences
 
 # Enter valid paths from your own file system
 
-# The input file contains names as a line seperated list
-input_txt_file = 'D:\Path-to-your-input-file\certi-namelist.txt'
+# The input file contains names as a comma seperated list of format (Rank, User handle, Name)
+participants = pd.read_csv("ranklist.csv")
 
-template_file_path = 'D:\Path-to-your-own-template-image\CERTI_TEMPLATE.png'
+template_file_path = "CERTI_TEMPLATE.png"
 
 # Make sure this output directory already exists or else certificates won't actually be generated
-output_directory_path = 'D:\Path-for-output-directory\Generated_Certificates\\'
+output_directory_path = "generated_certificates"
 
 font_size = 3.8
 font_color = (51,51,51)
@@ -26,32 +24,35 @@ coordinate_y_adjustment = 280
 # X adjustment determiens the px to position to the right of verticial center of the template (may be positive or negative)
 coordinate_x_adjustment = 7
 
-# Core Logic begins
+disqualified = {"user_handle_of_disqualified"}
 
-print('The Script is running...')
+winners = {"user_handle_of_winners"}
 
-with open(input_txt_file) as input_list:
-    
-    content = input_list.read().splitlines()
+for index, row in participants.iterrows():
 
-    for line in content:
+    if row["User handle"] in disqualified or row["User handle"] in winners:
+        continue
 
-        certi_name = line
+    print(row["User handle"])
 
-        img = cv2.imread(template_file_path)
+    certiName = row["Name"].title()
 
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        text = certi_name
-    
-        textsize = cv2.getTextSize(text, font, font_size, 10)[0]
-        text_x = (img.shape[1] - textsize[0]) / 2 + coordinate_x_adjustment
-        text_y = (img.shape[0] + textsize[1]) / 2 - coordinate_y_adjustment
-        text_x = int(text_x)
-        text_y = int(text_y)
-		
-        cv2.putText(img, text, (text_x, text_y ), font, font_size, font_color, 10)
-        certi_path = output_directory_path + certi_name + '.png'
-        cv2.imwrite(certi_path,img)
+    img = cv2.imread(template_file_path)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text = certiName
+
+	textsize = cv2.getTextSize(text, font, font_size, 10)[0]
+	text_x = (img.shape[1] - textsize[0]) / 2 + coordinate_x_adjustment
+	text_y = (img.shape[0] + textsize[1]) / 2 - coordinate_y_adjustment
+	text_x = int(text_x)
+	text_y = int(text_y)
+	
+	cv2.putText(img, text, (text_x, text_y ), font, font_size, font_color, 10)
+	certi_path = output_directory_path + certi_name + '.png'
+	cv2.imwrite(certi_path,img)
+
+    certiPath = "{}/{}.png".format(output_directory_path,row["User handle"])
+    cv2.imwrite(certiPath, img)
 
 cv2.destroyAllWindows()
-
